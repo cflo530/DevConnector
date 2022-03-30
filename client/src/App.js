b606d5cd6f2e6270380ea638bc84462824717051
Carlos Flores <cflo530@me.com>
2022-03-29 17:26:56 -0700

    Finished with Posts and Comments UI

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -60,6 +62,14 @@ const App = () => {
               <Route
                 path="add-education"
                 element={<PrivateRoute component={AddEducation} />}
+              />
+              <Route
+                path="/posts"
+                element={<PrivateRoute component={Posts} />}
+              />
+              <Route
+                path="/posts/:id"
+                element={<PrivateRoute component={Post} />}
               />
             </Routes>
         </Fragment>
